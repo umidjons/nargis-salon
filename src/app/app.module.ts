22179965be8e355182eb1dd5b810d636be242190
umidjons <almatov.us@gmail.com>
2018-01-06 20:31:24 +0500

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
