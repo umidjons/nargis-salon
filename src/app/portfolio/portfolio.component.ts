947eff67bd50d176c93050e5ff25d99b92ca3ee4
umidjons <almatov.us@gmail.com>
2018-01-16 20:55:31 +0500

    Did more responsive.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ns-portfolio',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  items: IPortfolio[];
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.items = [
+      {
+        image: 'nail.jpg',
+        title: 'Наращивание ногтей'
+      },
+      {
+        image: 'portfolio-8.jpg',
+        title: 'Наращивание волосы холодное микро, средние, большое капсулы'
+      },
+      {
+        image: 'portfolio-1.jpg',
+        title: 'Увеличение губ гелем'
+      },
+      {
+        image: 'portfolio-3.jpg',
+        title: 'Лазерное удаление тата и татуаж'
+      },
+      {
+        image: 'portfolio-7.jpg',
+        title: 'Лазерное удаление татуировок'
+      },
+      {
+        image: 'portfolio-4.jpg',
+        title: 'Наращивание ресницы 2D/3D'
+      },
+      {
+        image: 'portfolio-5.jpg',
+        title: 'Растушевка и татуаж'
+      },
+      {
+        image: 'portfolio-6.jpg',
+        title: 'Растушевка и татуаж'
+      },
+    ];
+  }
+
+}
+
+interface IPortfolio {
+  image: string;
+  title: string;
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ns-services',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  items: IService[];
+
+  constructor() {
+    this.items = [
+      {
+        image: 'srv6.png',
+        title: 'Парикмахерская',
+        description: 'Мелирование, укладки, стрижка и наращивание волос'
+      },
+      {
+        image: 'srv2.png',
+        title: 'Ногтевые услуги',
+        description: 'Наращивание ногтей гелем, дизайн, маникюр, шеллак'
+      },
+      {
+        image: 'srv3.png',
+        title: 'Ресницы и растушевка',
+        description: 'Наращивание ресниц, растушёвка'
+      },
+      {
+        image: 'srv4.png',
+        title: 'Удаление тату',
+        description: 'Лазерное удаление тату и растушевка'
+      },
+      {
+        image: 'srv1.png',
+        title: 'Уход за бородой',
+        description: 'Отрастить и стричь бороду, уход за бородой'
+      },
+    ];
+  }
 
   ngOnInit() {
   }
 
+}
+
+interface IService {
+  image: string;
+  title: string;
+  description: string;
 }
